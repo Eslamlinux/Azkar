8cc3b086a546dcbdbdbce8ed0918d54520eedd28
Eslam Linux <linuxeslam@gmail.com>
2025-10-22 19:42:44 +0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // هذا السطر هو الأهم: يخبر Next.js بتصدير الموقع كملفات ثابتة
+  output: 'export', 
+  
+  // (اختياري) هذا السطر ضروري إذا كنت تنشر على مسار فرعي مثل /Azkar/
+  basePath: '/Azkar', 
+  
+  // (اختياري) تأكد من تعيينه إلى true
+  trailingSlash: true,
+
   eslint: {
     ignoreDuringBuilds: true,
   },
