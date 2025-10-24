d47949120aada866afdc3f66dcce30096e82073d
Eslamlinux <linuxeslam@gmail.com>
2025-10-24 03:02:11 +0300

    edit ClientLayout

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -2,7 +2,11 @@
 
 import type React from "react"
 import { Amiri, Cairo } from "next/font/google"
-
+/**
+ * تحميل خطوط Google Fonts للغة العربية
+ * Amiri: خط تقليدي جميل للنصوص العربية الطويلة
+ * Cairo: خط حديث وواضح للعناوين والنصوص القصيرة
+ */
 const amiri = Amiri({
   subsets: ["arabic", "latin"],
   weight: ["400", "700"],
